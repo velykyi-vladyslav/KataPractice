5c61da157f0bc273a0fbe45d119eb1d36841cbf3
Vladyslav <vladyslav.velykyi.777@gmail.com>
2023-03-28 20:40:07 +0300

    First commit

---
+++ src/main/java/velykyi/vladyslav/App.java
@@ -0,0 +1,4 @@
+package velykyi.vladyslav;
+
+public class App {
+}
---
+++ src/main/java/velykyi/vladyslav/level1/KataApp.java
@@ -0,0 +1,6 @@
+package velykyi.vladyslav.level1;
+
+public class KataApp {
+    public static void main(String[] args) {
+    }
+}
