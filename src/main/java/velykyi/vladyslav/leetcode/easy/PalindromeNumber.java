8c23084816f598753567d33b6402ce83bb867b65
Vladyslav Velykyi <vladyslav.velykyi.777@gmail.com>
2023-07-20 06:32:38 +0300

    Palindrome Number init

---
+++ src/main/java/velykyi/vladyslav/leetcode/easy/PalindromeNumber.java
@@ -0,0 +1,13 @@
+package velykyi.vladyslav.leetcode.easy;
+
+/**
+ * Leetcode task: <a href="https://leetcode.com/problems/palindrome-number">palindrome-number/a>
+ */
+public class PalindromeNumber {
+
+    public boolean isPalindrome(int x) {
+
+
+        return false;
+    }
+}
