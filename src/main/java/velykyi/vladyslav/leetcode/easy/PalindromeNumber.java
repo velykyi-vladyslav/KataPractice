fa04ff441e76f5682d30e4420741145f69252335
Vladyslav Velykyi <vladyslav.velykyi.777@gmail.com>
2023-07-21 07:04:54 +0300

    Palindrome Number solution

--- src/main/java/velykyi/vladyslav/leetcode/easy/PalindromeNumber.java
+++ src/main/java/velykyi/vladyslav/leetcode/easy/PalindromeNumber.java
@@ -5,9 +5,30 @@ package velykyi.vladyslav.leetcode.easy;
  */
 public class PalindromeNumber {
 
-    public boolean isPalindrome(int x) {
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(0));
+    }
 
 
-        return false;
+    public static boolean isPalindrome(int x) {
+        // 0 - palindrome
+        if (x == 0) {
+            return true;
+        }
+        // -1, 0001 - is not palindrome
+        if (x < 0 || x % 10 == 0) {
+            return false;
+        }
+
+        int reversedX = 0;
+        while (x > reversedX) {
+            int pop = x % 10;
+            x /= 10;
+            reversedX = (reversedX * 10) + pop;
+        }
+
+        // 1221 -> 12 == 12 || 121 -> 1 == 12 / 10
+        return x == reversedX || x == (reversedX / 10);
     }
 }
